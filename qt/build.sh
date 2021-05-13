7ba6e980b8b9b2791ebde8090f34a8e937f5474a
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-05-13 22:15:10 +0000

    Copy some packages from the original recipe

--- qt/build.sh
+++ qt/build.sh
@@ -3,5 +3,20 @@ set -exou
 mkdir qt-build
 pushd qt-build
 
-../configure -opensource -nomake examples -nomake tests -gstreamer 1.0 -skip qtwebengine
+USED_BUILD_PREFIX=${BUILD_PREFIX:-${PREFIX}}
+echo USED_BUILD_PREFIX=${BUILD_PREFIX}
+
+ln -s ${GXX} g++ || true
+ln -s ${GCC} gcc || true
+ln -s ${USED_BUILD_PREFIX}/bin/${HOST}-gcc-ar gcc-ar || true
+
+export LD=${GXX}
+export CC=${GCC}
+export CXX=${GXX}
+
+chmod +x g++ gcc gcc-ar
+export PATH=${PWD}:${PATH}
+
+../configure -opensource -nomake examples -nomake tests -gstreamer 1.0 -skip qtwebengine -confirm-license
+make -j$(nproc)
 exit 1
