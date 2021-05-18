c2fe4e6ff49adb244420b1108964187f2f6d4768
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-05-18 15:55:48 -0500

    Add basic qtwebengine recipe

---
+++ qtwebengine/build.sh
@@ -0,0 +1,29 @@
+set -exou
+
+mkdir qtwebengine-build
+pushd qtwebengine-build
+
+USED_BUILD_PREFIX=${BUILD_PREFIX:-${PREFIX}}
+echo USED_BUILD_PREFIX=${BUILD_PREFIX}
+
+ln -s ${GXX} g++ || true
+ln -s ${GCC} gcc || true
+ln -s ${USED_BUILD_PREFIX}/bin/${HOST}-gcc-ar gcc-ar || true
+
+export LD=${GXX}
+export CC=${GCC}
+export CXX=${GXX}
+
+chmod +x g++ gcc gcc-ar
+export PATH=${PWD}:${PATH}
+
+# Set QMake prefix to $PREFIX
+qmake -set prefix $PREFIX
+
+qmake QMAKE_LIBDIR=${PREFIX}/lib \
+      QMAKE_LFLAGS+="-Wl,-rpath,$PREFIX/lib -Wl,-rpath-link,$PREFIX/lib -L$PREFIX/lib" \
+      INCLUDEPATH+="${PREFIX}/include" \
+      ..
+
+make -j$(nproc)
+make install
