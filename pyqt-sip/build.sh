4832b06c90fb6199825ecff34f3cbd88a962bfe5
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-05-24 14:12:30 -0500

    PyQt5: Add build script

---
+++ pyqt-sip/build.sh
@@ -0,0 +1,18 @@
+set -exou
+
+USED_BUILD_PREFIX=${BUILD_PREFIX:-${PREFIX}}
+echo USED_BUILD_PREFIX=${BUILD_PREFIX}
+
+ln -s ${GXX} g++ || true
+ln -s ${GCC} gcc || true
+ln -s ${USED_BUILD_PREFIX}/bin/${HOST}-gcc-ar gcc-ar || true
+
+export LD=${GXX}
+export CC=${GCC}
+export CXX=${GXX}
+export PKG_CONFIG_EXECUTABLE=$(basename $(which pkg-config))
+
+chmod +x g++ gcc gcc-ar
+export PATH=${PWD}:${PATH}
+
+$PYTHON -m pip install . -vv
