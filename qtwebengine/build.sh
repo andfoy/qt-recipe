9190ccc7fdadfd606c6b6eb048800afe877dda58
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-06-14 13:47:02 -0500

    QtWebEngine [MacOS/all]: Update recipe

--- qtwebengine/build.sh
+++ qtwebengine/build.sh
@@ -27,32 +27,61 @@ pushd qtwebengine-build
 USED_BUILD_PREFIX=${BUILD_PREFIX:-${PREFIX}}
 echo USED_BUILD_PREFIX=${BUILD_PREFIX}
 
-ln -s ${GXX} g++ || true
-ln -s ${GCC} gcc || true
-ln -s ${USED_BUILD_PREFIX}/bin/${HOST}-gcc-ar gcc-ar || true
+if [[ $(uname) == "Linux" ]]; then
+    ln -s ${GXX} g++ || true
+    ln -s ${GCC} gcc || true
+    ln -s ${USED_BUILD_PREFIX}/bin/${HOST}-gcc-ar gcc-ar || true
 
-export LD=${GXX}
-export CC=${GCC}
-export CXX=${GXX}
+    export LD=${GXX}
+    export CC=${GCC}
+    export CXX=${GXX}
 
-chmod +x g++ gcc gcc-ar
-export PATH=$PREFIX/bin:${PWD}:${PATH}
+    chmod +x g++ gcc gcc-ar
+    export PATH=$PREFIX/bin:${PWD}:${PATH}
 
-which pkg-config
-export PKG_CONFIG_EXECUTABLE=$(which pkg-config)
+    which pkg-config
+    export PKG_CONFIG_EXECUTABLE=$(which pkg-config)
 
-$(gcc -print-file-name=libc.so.6)
+    $(gcc -print-file-name=libc.so.6)
 
-# Set QMake prefix to $PREFIX
-qmake -set prefix $PREFIX
+    # Set QMake prefix to $PREFIX
+    qmake -set prefix $PREFIX
 
-qmake QMAKE_LIBDIR=${PREFIX}/lib \
-      QMAKE_LFLAGS+="-Wl,-rpath,$PREFIX/lib -Wl,-rpath-link,$PREFIX/lib -L$PREFIX/lib" \
-      INCLUDEPATH+="${PREFIX}/include" \
-      PKG_CONFIG_EXECUTABLE=$(which pkg-config) \
-      ..
+    qmake QMAKE_LIBDIR=${PREFIX}/lib \
+        QMAKE_LFLAGS+="-Wl,-rpath,$PREFIX/lib -Wl,-rpath-link,$PREFIX/lib -L$PREFIX/lib" \
+        INCLUDEPATH+="${PREFIX}/include" \
+        PKG_CONFIG_EXECUTABLE=$(which pkg-config) \
+        ..
 
-#cat config.log
-#exit 1
-CPATH=$PREFIX/include:$BUILD_PREFIX/src/core/api make -j$(nproc)
-make install
+    #cat config.log
+    #exit 1
+    CPATH=$PREFIX/include:$BUILD_PREFIX/src/core/api make -j$(nproc)
+    make install
+fi
+
+if [[ $(uname) == "Darwin" ]]; then
+    export AR=$(basename ${AR})
+    export RANLIB=$(basename ${RANLIB})
+    export STRIP=$(basename ${STRIP})
+    export OBJDUMP=$(basename ${OBJDUMP})
+    export CC=$(basename ${CC})
+    export CXX=$(basename ${CXX})
+
+    # Let Qt set its own flags and vars
+    for x in OSX_ARCH CFLAGS CXXFLAGS LDFLAGS
+    do
+        unset $x
+    done
+
+    # Set QMake prefix to $PREFIX
+    qmake -set prefix $PREFIX
+
+    qmake QMAKE_LIBDIR=${PREFIX}/lib \
+        QMAKE_LFLAGS+="-Wl,-rpath,$PREFIX/lib -Wl,$PREFIX/lib -L$PREFIX/lib" \
+        INCLUDEPATH+="${PREFIX}/include" \
+        PKG_CONFIG_EXECUTABLE=$(which pkg-config) \
+        ..
+
+    make -j$CPU_COUNT
+    make install
+fi
