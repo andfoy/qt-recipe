2ae0e18eca6645993663ada2a98f14ee555de613
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-12-07 00:55:11 -0500

    Remove extra flags in tests

--- qtwebengine-feedstock/recipe/run_test.sh
+++ qtwebengine-feedstock/recipe/run_test.sh
@@ -7,8 +7,5 @@ cd test
 ln -s ${GXX} g++
 export PATH=$PREFIX/bin/xc-avoidance:${PWD}:${PATH}
 # Only test that this builds
-qmake QMAKE_LIBDIR=${PREFIX}/lib \
-      INCLUDEPATH+="${PREFIX}/include" \
-      QMAKE_LFLAGS+="-Wl,-rpath,$PREFIX/lib -Wl,-rpath-link,$PREFIX/lib -L$PREFIX/lib" \
-      qtwebengine.pro
+qmake qtwebengine.pro
 make
